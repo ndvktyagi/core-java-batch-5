42026c08c5beb368de33e49d666b6f195ec8b282
ndvktyagi <vishaltyagi11187@gmail.com>
2022-02-04 08:07:35 +0530

    04.02.2022 code committed

---
+++ src/main/java/basicConcept/MyFirstJavaClass.java
@@ -0,0 +1,7 @@
+package basicConcept;
+
+public class MyFirstJavaClass {
+    public static void main(String args[]){
+        System.out.println("This is my first Java Program for testing it is running or not.");
+    }
+}
